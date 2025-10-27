034e115e7531b82e291d2cbbc721b2b6b276d65d
clintlee93 <clintonleemunyiri@gmail.com>
2025-10-27 11:29:04 +0300

    day 1 creating all the folders awaiting for content creation

---
+++ cosy_cup_cafe/src/components/header.jsx
@@ -0,0 +1,16 @@
+
+function Header() {
+  return (
+     <ul>
+    <li><a href="home">Home</a></li>
+    <li><a href="about">About</a></li>
+    <li><a href="features">Features</a></li>
+    <li><a href="contacts">Contacts</a></li>  
+     </ul>
+  );
+}
+
+
+
+
+export default Header;
